170b928505bfc1e9d85ad57fa34ef3b0d0fc1a1f
sunsea22 <418664438@qq.com>
2016-05-06 10:04:59 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "metroLinePrediction"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.5.1",
+  "org.apache.spark" %% "spark-sql" % "1.5.1",
+  "org.apache.spark" %% "spark-mllib" % "1.5.1"
+)
+    
---
+++ src/main/scala/subway/LocalClass.scala
@@ -0,0 +1,10 @@
+package subway
+
+/**
+ * Created by Flyln on 16/5/4.
+ */
+
+case class passengerTripList(cardId:String,departureStation:String,arriveStation:String,residence:String,weekNum:String,departureTime:String,tripList:String)
+
+
+case class passengerTripListAddFeatures(cardId:String,departureStation:String,arriveStation:String,residence:String,weekNum:String,departureTime:String,tripList:String,labelNew:String,fourFeatures:String)
